b6442920b883d0fd6d4ad4c23f6946394c07b0b8
DavideZattra <d.zattra@gmail.com>
2021-10-06 22:49:09 +0200

    created main subscribe section

--- src/components/MainSubscribe.vue
+++ src/components/MainSubscribe.vue
@@ -1,13 +1,41 @@
 <template>
-  <section></section>
+  <section id="ms_subscribe" class="text-center">
+      <h2>Subscribe <span>&amp;</span> Receive A Free eBook</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In exercitationem, sunt saepe dolor quas reprehenderit?</p>
+      <input type="text" placeholder="Insert your email...*">
+      <button class="btn">Subscribe</button>
+      <pre>Lorem ipsum dolor sit amet consectetur adipisicing elit.</pre>
+  </section>
 </template>
 
 <script>
 export default {
-
+name : 'Subscribe',
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+@import '../style/variables.scss';
+
+#ms_subscribe{
+    padding: 25px 0;
+    background-color: $mainWhiteColor;
+    background-image: url('../assets/img/subscribe-sec-bg.png');
+    background-position: center;
+    background-size: cover;
+
+    input{
+        padding: 0;
+        line-height: 33px;
+    }
+
+    button{
+        margin-left: 15px;
+        color: white;
+        text-transform: uppercase;
+        background-color: $mainOrangeColor;
+        border-radius: 0;
+    }
+}
 
 </style>
