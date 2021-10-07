81ab499c6b9c0222cd5b0b61b564b38e186d173a
DavideZattra <d.zattra@gmail.com>
2021-10-07 14:36:37 +0200

    fixed footer positioning and added top-button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,10 @@
 <template>
   <footer>
-    <section id="ms_upper-footer" class="container-fluid">
+    <!-- section with all info about avada social links and store link -->
+    <section id="ms_upper-footer" class="container">
       <div class="row justify-content-center pt-5 pb-5">
-        <div class="col-2">
-          <a href="#"><img src="../assets/img/avada-food-logo.png" alt="Main Avada Logo"></a>
+        <div id="ms_social" class="col-3 border-end">
+          <a href="#"><img class="" src="../assets/img/avada-food-logo-mob.png" alt="Main Avada Logo"></a>
           <ul class="d-flex p-0">
 
             <li class="pt-2 ps-2 pe-2"><a href="#"></a><i class="fab fa-facebook"></i></li>
@@ -13,14 +14,14 @@
 
           </ul>
         </div>
-        <div class="col-2">
+        <div class="col-3 border-end">
           <ul>
             <li class="text-capitalize pb-2" v-for="(element, index) in sectionList" :key="index">
               <a :href="element.url"> {{ element.name }}</a>
             </li>
           </ul>
         </div>
-        <div class="col-2">
+        <div id="ms_contacts" class="col-3 border-end">
           <h5>contact</h5>
           <ul class="p-0">
             <li class="d-flex">
@@ -38,21 +39,20 @@
             </li>
           </ul>
         </div>
-        <div class="col-2">
+        <div id="ms_appStore" class="col-3 ">
           <h5>get our app!</h5>
-          <ul>
-            <li>
-              <a href="#"><img class="img-fluid" src="../assets/img/download-app-store.png" alt=""></a>
-            </li>
-            <li>
-              <a href="#"><img class="img-fluid" src="../assets/img/download-android.png" alt=""></a>
-            </li>
-          </ul>
+          <img class="img-fluid" src="../assets/img/download-android.png" alt="">
+          <img class="img-fluid" src="../assets/img/download-app-store.png" alt="">
+          
         </div>
       </div>
 
     </section>
+
+    <!-- Copyright Section and button to go on the top of page -->
     <section id="ms_lower-footer" class="text-center">
+
+      <a href="#">&uarr;</a>
       
       <p>&copy; Copyright 2012 - 2020 | Avada Theme by ThemeFusion | All Rights Reserved | Powered by WordPress</p>
       
@@ -73,27 +73,67 @@ components:{
 <style lang='scss' scoped>
 @import '../style/variables.scss';
 footer{
-  background-color: ivory;
+  background-color: white;
+
+
+  #ms_upper-footer{
+    h5{
+      font-size: 0.9rem;
+      text-transform: uppercase;
+      color: $mainOrangeColor;
+    }
+    #ms_social{
+      img{
+        width: 90%;
+      }
+      i{
+        padding-top: 25px;
+        color: $mainDarkGrayColor;
+      }
+    }
+  
+    #ms_appStore{
+      img{
+        width: 90%;
+        padding-bottom: 10px;
+      }
+    }
+    #ms_contacts{
+
+      color: $mainDarkGrayColor;
+      i{
+      padding-right: 10px;
+    }
+    }
+  }
+
+  #ms_lower-footer{
+    position: relative;
+    background-color: $mainLightGrayColor;
+    height: 50px;
+
+    a{
+      position: absolute;
+      top: 5px;
+      left: 25px;
+      display: inline-block;
+
+      height: 35px;
+      width: 35px;
+      color: white;
+      background-color: $mainDarkGrayColor;
+      border-radius: 50%;
+      line-height: 35px;
+
+    }
+
+    p{
+      font-size: 0.5rem;
+      line-height: 50px;
+      margin: 0;
+    }
+  }
 }
 
-#ms_upper-footer{
-  h5{
-    text-transform: uppercase;
-    color: $mainOrangeColor;
-  }
-  i{
-    padding-right: 10px;
-  }
-}
-
-#ms_lower-footer{
-  background-color: $mainLightGrayColor;
-  height: 50px;
-
-  p{
-    line-height: 50px;
-    margin: 0;
-  }
-}
 
 </style>
