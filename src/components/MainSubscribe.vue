17836f5b2669a74cf7b87b99e65dd5c5425986eb
DavideZattra <d.zattra@gmail.com>
2021-10-08 13:31:57 +0200

    added image slider to subscribe section

--- src/components/MainSubscribe.vue
+++ src/components/MainSubscribe.vue
@@ -1,11 +1,28 @@
 <template>
-  <section id="ms_subscribe" class="text-center">
-      <h2>Subscribe <span>&amp;</span> Receive A Free eBook</h2>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In exercitationem, sunt saepe dolor quas reprehenderit?</p>
-      <input type="text" placeholder="Insert your email...*">
-      <button class="btn">Subscribe</button>
-      <pre>Lorem ipsum dolor sit amet consectetur adipisicing elit.</pre>
-  </section>
+    <div>
+        <section id="ms_subscribe" class="text-center">
+            <h2>Subscribe <span>&amp;</span> Receive A Free eBook</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In exercitationem, sunt saepe dolor quas reprehenderit?</p>
+            <input type="text" placeholder="Insert your email...*">
+            <button class="btn">Subscribe</button>
+            <pre>Lorem ipsum dolor sit amet consectetur adipisicing elit.</pre>
+        </section>
+        <section class="ms_image-slider">
+            <div class="slider-img-container">
+                <img src="../assets/img/Yogurt-Nan.jpg" alt="">
+            </div>
+            <div class="slider-img-container">
+                <img src="../assets/img/Mixed-fruits.jpg" alt="">
+            </div>
+            <div class="slider-img-container">
+                <img src="../assets/img/r-rachel-park-366508-unsplash-min.jpg" alt="">
+            </div>
+            <div class="slider-img-container">
+                <img src="../assets/img/r-michelle-tsang-500721-unsplash-min.jpg" alt="">
+            </div>
+
+        </section>
+    </div>
 </template>
 
 <script>
@@ -38,4 +55,24 @@ name : 'Subscribe',
     }
 }
 
+.ms_image-slider{
+    display: flex;
+    .slider-img-container{
+        overflow: hidden;
+        img{
+            width: 100%;
+            transition: transform 1s, filter 1s ease-in-out;
+            filter: blur(2px);
+            transform: scale(1.1)
+
+        }
+
+            img:hover{
+            
+            filter: blur(0);
+            transform: scale(1);
+        }
+    }
+
+}
 </style>
